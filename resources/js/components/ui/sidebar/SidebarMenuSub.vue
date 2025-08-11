812fe79e4aea7ec5734f3da915cc37962b4408e6
Max Kunz <68565651+maxxkunz@users.noreply.github.com>
2025-08-12 00:50:25 +0200

    Neue Sidebar07 von shadcn/vue (A sidebar that collapses to icons) inklusive funktionierender Routen

--- resources/js/components/ui/sidebar/SidebarMenuSub.vue
+++ resources/js/components/ui/sidebar/SidebarMenuSub.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from "vue"
+import { cn } from "@/lib/utils"
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
+  class?: HTMLAttributes["class"]
 }>()
 </script>
 
@@ -12,7 +12,7 @@ const props = defineProps<{
     data-slot="sidebar-menu-sub"
     data-sidebar="menu-badge"
     :class="cn(
-      'border-sidebar-border mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l px-2.5 py-0.5',
+      'border-sidebar-border  flex min-w-0 translate-x-px flex-col gap-1 border-l pl-2.5 py-0.5',
       'group-data-[collapsible=icon]:hidden',
       props.class,
     )"
